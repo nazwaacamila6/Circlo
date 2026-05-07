6b2886a40fd7234f855e7ed6341fbb793fdb5432
breliandwioktaviani <brelianoktaviani@gmail.com>
2026-05-07 09:38:15 +0700

    update package home

--- app/src/main/java/com/example/circlo/home/HomeScreen.kt
+++ app/src/main/java/com/example/circlo/home/HomeScreen.kt
@@ -1,7 +1,26 @@
 package com.example.circlo.home
 
-data class HomeScreen (
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+data class Event (
     val title : String,
     val date : String,
     val location : String
 )
+
+@Composable
+fun HomeScreen() {
+    val eventList = listOf(
+        Event(
+            title = "Green Community Walk",
+            date = "Oct 24, 5:00 PM",
+            location = "Jakarta Selatan"
+        ),
+        Event(
+            title = "Acoustic Garden Night",
+            date = "Oct 26, 7:00 PM",
+            location = "Cipete Jakarta Selatan"
+        )
+    )
+}
